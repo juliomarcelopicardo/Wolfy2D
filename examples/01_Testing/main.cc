72a7e778853189c06d563a4d740c63aa5743c531
Julio Marcelo Picardo <juliomarcelopicardo@gmail.com>
2017-10-16 03:09:35 +0100

    EXAMPLE testing with all the inputs. Working.

--- examples/01_Testing/main.cc
+++ examples/01_Testing/main.cc
@@ -16,7 +16,7 @@ int32 main() {
 
   while (Window::IsOpened()) {
     Window::Clear();
-    
+    if (Input::IsKeyboardButtonDown(Input::kKeyboardButton_W)) {      printf("\n W down");    }    if (Input::IsKeyboardButtonPressed(Input::kKeyboardButton_S)) {      printf("\n S pressed");    }    if (Input::IsKeyboardButtonUp(Input::kKeyboardButton_D)) {      printf("\n D up");    }    if (Input::IsMouseButtonDown(Input::kMouseButton_Left)) {      printf("\n Left down");    }    if (Input::IsMouseButtonPressed(Input::kMouseButton_Middle)) {      printf("\n Middle pressed");    }    if (Input::IsMouseButtonUp(Input::kMouseButton_Right)) {      printf("\n Right up");    }    
     Window::Frame();
   }
 
