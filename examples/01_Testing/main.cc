3ae5c634298fac1638187c8bbfbef127cf0848da
Julio Marcelo Picardo <juliomarcelopicardo@gmail.com>
2017-10-16 01:18:19 +0100

    EXAMPLE added basic example with an empty window

---
+++ examples/01_Testing/main.cc
@@ -0,0 +1,28 @@
+/** Copyright Julio Marcelo Picardo. SouthBros 2017-18, all rights reserved.
+*
+*  @project Wolfy2D
+*  @author Julio Marcelo Picardo <juliomarcelopicardo@gmail.com>
+*
+*/
+
+ 
+#include <string>
+#include "Wolfy2D.h"
+
+namespace W2D {
+int32 main() {
+  
+  Window::Init(1024, 978);
+
+  while (Window::IsOpened()) {
+    Window::Clear();
+
+    Window::Frame();
+  }
+
+  Window::Close();
+
+  return 0;
+}
+
+}; /* W2D */
