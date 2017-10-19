e43f338556496b597000b148e8df370286da651a
Julio Marcelo Picardo <juliomarcelopicardo@gmail.com>
2017-10-20 00:24:00 +0100

    INPUT code commented

--- include/Wolfy2D/input.h
+++ include/Wolfy2D/input.h
@@ -12,11 +12,13 @@
 namespace W2D {
 namespace Input {
 
+/// Mouse buttons enum.
 enum MouseButton {
   kMouseButton_Left = 0,
   kMouseButton_Right = 1,
   kMouseButton_Middle = 2,
 };
+/// Keyboard buttons enum.
 enum KeyboardButton {
   kKeyboardButton_W = 87,
   kKeyboardButton_A = 65,
@@ -27,12 +29,60 @@ enum KeyboardButton {
   kKeyboardButton_Tab = 258,
 };
 
+///--------------------------------------------------------------------------
+/// @fn   glm::vec2 MousePosition();
+///
+/// @brief Mouse cursor position getter.
+/// @return Position.
+///--------------------------------------------------------------------------
 glm::vec2 MousePosition();
+///--------------------------------------------------------------------------
+/// @fn   bool IsMouseButtonDown(MouseButton button);
+///
+/// @brief Mouse button down info.
+/// @param button Mouse button to check
+/// @return true if the button is down.
+///--------------------------------------------------------------------------
 bool IsMouseButtonDown(MouseButton button);
+///--------------------------------------------------------------------------
+/// @fn   bool IsMouseButtonUp(MouseButton button);
+///
+/// @brief Mouse button up info.
+/// @param button Mouse button to check
+/// @return true if the button is up.
+///--------------------------------------------------------------------------
 bool IsMouseButtonUp(MouseButton button);
+///--------------------------------------------------------------------------
+/// @fn   bool IsMouseButtonPressed(MouseButton button);
+///
+/// @brief Mouse button pressed info.
+/// @param button Mouse button to check
+/// @return true if the button is pressed.
+///--------------------------------------------------------------------------
 bool IsMouseButtonPressed(MouseButton button);
+///--------------------------------------------------------------------------
+/// @fn   bool IsKeyboardButtonDown(KeyboardButton button);
+///
+/// @brief Keyboard button down info.
+/// @param button Keyboard button to check
+/// @return true if the button is down.
+///--------------------------------------------------------------------------
 bool IsKeyboardButtonDown(KeyboardButton button);
+///--------------------------------------------------------------------------
+/// @fn   bool IsKeyboardButtonUp(KeyboardButton button);
+///
+/// @brief Keyboard button up info.
+/// @param button Keyboard button to check
+/// @return true if the button is up.
+///--------------------------------------------------------------------------
 bool IsKeyboardButtonUp(KeyboardButton button);
+///--------------------------------------------------------------------------
+/// @fn   bool IsKeyboardButtonPressed(KeyboardButton button);
+///
+/// @brief Keyboard button pressed info.
+/// @param button Mouse button to check
+/// @return true if the button is pressed.
+///--------------------------------------------------------------------------
 bool IsKeyboardButtonPressed(KeyboardButton button);
 
 }; /* Input */
