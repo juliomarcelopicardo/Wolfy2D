11df28caed5ce29a7a207a871c1708d656efdd65
Julio Marcelo Picardo <juliomarcelopicardo@gmail.com>
2017-10-21 18:51:51 +0100

    [DRAW] Draw::Line function

---
+++ include/Wolfy2D/draw.h
@@ -0,0 +1,35 @@
+/** Copyright Julio Marcelo Picardo 2017-18, all rights reserved.
+*
+*  @project Wolfy2D
+*  @author Julio Marcelo Picardo <juliomarcelopicardo@gmail.com>
+*/
+
+#ifndef __WOLFY2D_DRAW_H__
+#define __WOLFY2D_DRAW_H__ 1
+
+#include "Wolfy2D/globals.h"
+
+namespace W2D {
+namespace Draw {
+
+///--------------------------------------------------------------------------
+/// @fn   Line(const Vec2 origin, const Vec2 destiny,
+///            const Vec4 color = { 1.0f, 1.0f, 1.0f, 1.0f },
+///            const float32 line_width = 2.0f);
+///
+/// @brief  Draws a line in the screen.
+/// @param  origin Line origin position.
+/// @param  destiny Line destiny position.
+/// @param  color Line color.
+/// @param  line_width Line width.
+///--------------------------------------------------------------------------
+void Line(const Vec2 origin, const Vec2 destiny,
+          const Vec4 color = { 1.0f, 1.0f, 1.0f, 1.0f },
+          const float32 line_width = 2.0f);
+
+
+
+}; /* Draw */
+}; /* W2D */
+
+#endif
---
+++ src/Wolfy2D/draw.cc
@@ -0,0 +1,27 @@
+/** Copyright Julio Marcelo Picardo 2017-18, all rights reserved.
+*
+*  @project Wolfy2D
+*  @author Julio Marcelo Picardo <juliomarcelopicardo@gmail.com>
+*/
+
+#include "Wolfy2D/draw.h"
+#include "GL/glew.h"
+
+namespace W2D {
+namespace Draw {
+
+void Line(const Vec2 origin, 
+          const Vec2 destiny, 
+          const Vec4 color, 
+          const float32 line_width) {
+
+  glLineWidth(line_width);
+  glColor4f(color.x, color.y, color.z, color.w);
+  glBegin(GL_LINES);
+  glVertex2f(origin.x, origin.y);
+  glVertex2f(destiny.x, destiny.y);
+  glEnd();
+}
+
+}; /* Draw */
+}; /* W2D */
