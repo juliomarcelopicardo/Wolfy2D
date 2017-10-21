c3293fb8183b37342f94078fdfb970deccf5bc7f
Julio Marcelo Picardo <juliomarcelopicardo@gmail.com>
2017-10-21 18:55:45 +0100

    [TEXTURE] repeat mode changed to clamp to edge.

--- src/core/texture.cc
+++ src/core/texture.cc
@@ -57,8 +57,9 @@ void Texture::init(const char* texture_path) {
     glGenTextures(1, &texture_id_);
     glBindTexture(GL_TEXTURE_2D, texture_id_);
 
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // Repeat or clamp to edge work.
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
