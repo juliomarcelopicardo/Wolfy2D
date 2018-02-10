0d08a11ac152e287bb9a1532cc80ae1a076f46df
Julio Marcelo Picardo <juliomarcelopicardo@gmail.com>
2018-02-10 00:47:50 +0000

    [TESTING] Changes working in real time :)

--- examples/01_Testing/main.cc
+++ examples/01_Testing/main.cc
@@ -14,40 +14,78 @@
 
 namespace W2D {
 
-void CreateSprite(std::vector<JMP::Value>& param) {
+void CreateSprite(std::vector<JMP::Value>& p) {
   auto& core = Core::instance();
-  if (core.texture_factory_.find(param[0].text_) == core.texture_factory_.end()) {
-    Texture texture;
-    texture.init(param[1].text_.c_str());
-    texture.set_pivot(kSpritePivotPoint_UpLeft);
-    texture.set_position({ 100.0f, 100.0f });
-    texture.set_size({ 100.0f, 100.0f });
-    core.texture_factory_[param[0].text_] = texture;
-  }
+  Texture texture;
+  texture.init(p[1].text_.c_str());
+  texture.set_pivot(kSpritePivotPoint_UpLeft);
+  texture.set_position({ 100.0f, 100.0f });
+  texture.set_size({ 100.0f, 100.0f });
+  core.texture_factory_[p[0].text_] = texture;
 }
 
-void RenderSprite(std::vector<JMP::Value>& param) {
+void RenderSprite(std::vector<JMP::Value>& p) {
   auto& core = Core::instance();
-  if (core.texture_factory_.find(param[0].text_) == core.texture_factory_.end()) {
-    printf("ERROR: Couldnt find sprite name : \"%s\"", param[0].text_.c_str());
+  if (core.texture_factory_.find(p[0].text_) == core.texture_factory_.end()) {
+    printf("ERROR: Couldnt find sprite name : \"%s\"", p[0].text_.c_str());
     return;
   }
-  core.texture_factory_[param[0].text_].render();
+  core.texture_factory_[p[0].text_].render();
 }
+
+void SpriteSetPosition(std::vector<JMP::Value>& p) {
+  auto& core = Core::instance();
+  if (core.texture_factory_.find(p[0].text_) == core.texture_factory_.end()) {
+    printf("ERROR: Couldnt find sprite name : \"%s\"", p[0].text_.c_str());
+    return;
+  }
+  glm::vec2 position = { p[1].float_, p[2].float_ };
+  if (p[1].type_ == JMP::kValueType_Integer) { position.x = (float32)p[1].integer_; }
+  if (p[2].type_ == JMP::kValueType_Integer) { position.y = (float32)p[2].integer_; }
+  core.texture_factory_[p[0].text_].set_position(position);
+}
+
+void SpriteSetSize(std::vector<JMP::Value>& p) {
+  auto& core = Core::instance();
+  if (core.texture_factory_.find(p[0].text_) == core.texture_factory_.end()) {
+    printf("ERROR: Couldnt find sprite name : \"%s\"", p[0].text_.c_str());
+    return;
+  }
+  glm::vec2 size = { p[1].float_, p[2].float_ };
+  if (p[1].type_ == JMP::kValueType_Integer) { size.x = (float32)p[1].integer_; }
+  if (p[2].type_ == JMP::kValueType_Integer) { size.y = (float32)p[2].integer_; }
+  core.texture_factory_[p[0].text_].set_size(size);
+}
+
+void SpriteSetRotation(std::vector<JMP::Value>& p) {
+  auto& core = Core::instance();
+  if (core.texture_factory_.find(p[0].text_) == core.texture_factory_.end()) {
+    printf("ERROR: Couldnt find sprite name : \"%s\"", p[0].text_.c_str());
+    return;
+  }
+  core.texture_factory_[p[0].text_].set_rotation(p[1].float_);
+}
+
 
 int32 main() {
 
+  float time = (float32)Time();
   JMP::Machine jmp;
   jmp.processFile("../scripts/config.jmp");
+  jmp.registerVariable("time", JMP::kValueType_Float, &time);
   jmp.registerFunction("CreateSprite", &CreateSprite);
   jmp.registerFunction("RenderSprite", &RenderSprite);
+  jmp.registerFunction("SpriteSetPosition", &SpriteSetPosition);
+  jmp.registerFunction("SpriteSetRotation", &SpriteSetRotation);
+  jmp.registerFunction("SpriteSetSize", &SpriteSetSize);
 
   Window::Init(jmp.getInteger("width", "Window"), jmp.getInteger("height", "Window"));
   jmp.runFunction("Init()");
 
   while (Window::IsOpened()) {
     Window::Clear();
-    jmp.runFunction("Update()");    Window::Frame();
+    time = (float32)Time() * 0.0001f;
+    jmp.runFunction("Update()");    if (Input::IsKeyboardButtonDown(Input::kKeyboardButton_SpaceBar)) {      jmp.reload();      jmp.runFunction("Init()");    }    Window::Frame();
   }
 
   Window::Close();
